64b035871e1f6e7e6434e46e9fea756ba9ccc6c4
Morsi Mohsen <47028111+morsimohsen@users.noreply.github.com>
2020-11-26 17:17:16 +0200

    Signed-off-by: Morsi Mohsen <47028111+morsimohsen@users.noreply.github.com>

--- Cars.php
+++ Cars.php
@@ -6,47 +6,86 @@
     <title>MotorKo</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
     
-    <link href="bootstrap-4.5.3-dist/css/bootstrap.min.css" rel="stylesheet"> 
-    <link href="css/style.css" rel="stylesheet"> 
+    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+    <link href="css/st.css" rel="stylesheet"> 
     
 </head>
-
 <body>
+  <header class="custom-nav">
+    <Div class="container-fluid back"> 
+      <nav class="navbar navbar-expand-lg navbar-dark static-top p-3">
+        <a class="navbar-brand" href="#">
+              <img class="logo" src="images/Logo.png" alt="">
+            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">News <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">New Cars <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">Car Reviews <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">Branches <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">About US <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item mr-4">
+                <a class="nav-link" href="#">Contact US<span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <button class="btn m-2 px-4 my-sm-0 border btn-st d-md-inline " type="submit"><i class="fas fa-sign-in-alt pr-3"></i>Log In</button>
+              <button class="btn m-2 px-4 my-sm-0 btn-st border d-md-inline" type="submit"><i class="fas fa-user-plus pr-3"></i>Sing Up</button>
+            </form>
+          </div>
+      </nav>
+    </Div>
+  </header>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+
+  <!-- d -->
+  <div id="header">
+        <div class="header-caption">
+            <div class="caption">
+                <span class="py-5"><span class="font-weight-bold wel">Welcome</span> to MotorKo</span>
+                <h1> <span class="element"></span></h1>
+                <p>MotorKo Auto Company was established in 1984 and became a pioneer in the field of cars.
+                    MotorKo Auto Company is developing its business in the Egyptian market to provide all the needs of the Egyptian consumer in the field of cars and the highest level of quality and through nearly 15 branches covering all Egypt.
+                    More than 25 brands of cars are presented under one roof and in cooperation with major banks and companies. 
+                    The insurance companies to achieve a unique service for its customers to maintain their precious trust which is our main asset.</p>
+            </div>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
-</nav>
+
+
+   <!-- btn for scroll up -->
+   <div class="btn-up">
+        <a class="btn btn-danger"><i class="fas fa-arrow-circle-up"></i></a>
+    </div>
+    <!-- end of scroll -->
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <!-- <script src="js/popper.min.js"></script> -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/typed.js"></script>
+    <!-- <script src="js/owl.carousel.min.js"></script> -->
+    <script src="js/aos.js"></script>
+
+  <script src="js/scr.js"></script>
 </body>
 </html>
