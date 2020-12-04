9c7106d4483cfe61367b7d4d783ff42b0ccd8fa6
Morsi Mohsen <47028111+morsimohsen@users.noreply.github.com>
2020-12-04 14:23:26 +0200

    Change The scroll time

--- js/scr.js
+++ js/scr.js
@@ -48,7 +48,7 @@ $(function() {
     
     $(".btn-up").click(()=> {
     
-        $("html").animate({ scrollTop: '0' }, 1000);
+        $("html").animate({ scrollTop: '0' }, 1200);
     
     });
     
